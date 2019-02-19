ab5744692b9c75fc92bb07146e1ded4b83735efd
m-signo <mirko_signorelli@hotmail.it>
2019-02-19 16:19:02 +0100

    Add files via upload

---
+++ simE/1E-data-generation-sbm-K-incr.R
@@ -0,0 +1,27 @@
+source('0-random-graph-simulations.R')
+set.seed(1234)
+case = 'E'
+n.subcases = 7
+nrep = 50
+
+pmatrix_1 = cbind(c(0.4, 0.1, 0.1), c(0.1, 0.5, 0.2), c(0.1, 0.2, 0.3))
+pmatrix_2 = cbind(c(0.3, 0.2, 0.2), c(0.2, 0.3, 0.1), c(0.2, 0.1, 0.4))
+block_vector = c(rep(1:3, each = 6), 1, 2)
+
+for (subcase in 1:n.subcases) {
+  v = 20
+  K1 = K2 = seq(6, 30, by = 4)[subcase]
+  K = K1 + K2
+  ncomp = 2 
+  for (h in 1:nrep) {
+    set.seed(h)
+    pop1 = vector('list', K1)
+    pop2 = vector('list', K2)
+    for (i in 1:K1) pop1[[i]] = random_SBM(v, block_vector, pmatrix = pmatrix_1)
+    for (i in 1:K2) pop2[[i]] = random_SBM(v, block_vector, pmatrix = pmatrix_2)
+    data.file = paste('data/', case, '/graphs-', case, subcase, '-', h, '.RData', sep = '')
+    save.image(data.file)
+  }
+  print(subcase)
+}
+
